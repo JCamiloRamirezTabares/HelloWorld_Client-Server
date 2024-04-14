1aed35583effdd6fd8280024e1c1971a99c72dac
JCamiloRamirezTabares <juancaram21@gmail.com>
2024-04-13 22:17:10 -0500

    Script was fixed

--- scripts/testingServerResponse.sh
+++ scripts/testingServerResponse.sh
@@ -1,23 +1,17 @@
 password=$(<../credentials/password.txt)
 
-<<<<<<< HEAD
-clients=("hgrid6" "hgrid7" "hgrid8" "hgrid9" "hgrid10"
-                "hgrid11" "hgrid13" "hgrid14" "hgrid17")
-
-=======
 # Definir los servidores y sus respectivos números de Fibonacci
 declare -A client_fib_numbers=(
-    ["hgrid6"]=46
+    ["hgrid6"]=49
     ["hgrid7"]=30
     ["hgrid8"]=10
     ["hgrid9"]=7
     ["hgrid10"]=25
     ["hgrid11"]=40
-    ["hgrid12"]=46
+    ["hgrid12"]=55
     ["hgrid13"]=30
-    ["hgrid15"]=25
+    ["hgrid15"]=21
 )
->>>>>>> 5bc7721ad08f70f9e2f514600dbfdd59a55926b1
 
 # Bucle sobre cada par clave-valor en el diccionario
 for client in "${!client_fib_numbers[@]}"; do
